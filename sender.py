3b54d8e14b659ca998b6dc84c3bbe9530c29b7ff
Alexander Balyshyn <balyszyn@gmail.com>
2018-12-22 00:00:00 +0200

    change client impl for testing purposes

--- sender.py
+++ sender.py
@@ -7,8 +7,8 @@ MESSAGE = b"Hello, World!"
 sock = socket.socket(socket.AF_INET,  # Internet
 
                      socket.SOCK_DGRAM)  # UDP
-while True:
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    print('waiting to receive')
-    data, server = sock.recvfrom(4096)
-    print('received "%s"' % data)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+print('waiting to receive')
+data, server = sock.recvfrom(4096)
+print('received "%s"' % data)
