31c89d0e592474f053798e265712fef23c6ce881
limpid-kzonix <balyszyn@gmail.com>
2018-12-25 00:00:00 +0200

    fix error with global context of event_loop

--- udp_server.py
+++ udp_server.py
@@ -1,32 +1,41 @@
 import logging
-import multiprocessing
 import socket
 import asyncio
+
+"""
+
+"""
+loop = asyncio.get_event_loop()
+"""
+
+"""
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 
 
 class UdpServer(object):
     """
 
     """
-    event_loop = asyncio.get_running_loop()
     logger = None
-    socket = None
     hostname = str
     port = int
+    event_loop = loop
+    server_sock = sock
 
-    def __init__(self):
-        self.__init__("localhost", 30981)
-
-    def __init__(self, hostname: str, port: int):
+    def __init__(self, hostname: str = "localhost", port: int = 3553):
         self.hostname = hostname
         self.port = port
         self.logger = self.__get_default_logger()
+        self.__init_socket()
 
     def start(self):
         try:
-            self.event_loop.run_until_complete(self.__start())
+            loop.run_until_complete(self.__start())
         finally:
-            self.event_loop.close()
+            loop.close()
+
+    def stop(self):
+        self.event_loop.close()
 
     def __init_socket(self):
         """
@@ -34,37 +43,37 @@ class UdpServer(object):
         :return:
         """
         self.logger.debug("Listening on %s:%d", self.hostname, self.port)
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.setblocking(False)
-        self.socket.bind((self.hostname, self.port))
+        self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_sock.setblocking(False)
+        self.server_sock.bind((self.hostname, self.port))
 
     async def __start(self):
-        self.__init_socket()
-        while True:
-            data, addr = await self.__receive_from(1024)
-            #
-            n_bytes = await self.__send_to(data, addr)
+        """
 
-    # TODO: add logging of input messages and basic routing implementation
-    def __receive_from(self, n_bytes, future=None, registered=False):
-        fd = self.socket.fileno()
+        :return:
+        """
+        while True:
+            data, addr = await self.__recvfrom(n_bytes=1024)
+            self.logger.info("Message %r from %r ", data, addr)
+            await self.__sendto(data, addr)
+
+    def __recvfrom(self, n_bytes, future=None, registered=False):
+        fd = self.server_sock.fileno()
         if future is None:
             future = self.event_loop.create_future()
         if registered:
             self.event_loop.remove_reader(fd)
 
         try:
-            data, addr = self.socket.recvfrom(n_bytes)
+            data, addr = self.server_sock.recvfrom(n_bytes)
         except (BlockingIOError, InterruptedError):
-            self.event_loop.add_reader(fd, self.__receive_from, self.event_loop, self.socket, n_bytes, future, True)
+            self.event_loop.add_reader(fd, self.__recvfrom, n_bytes, future, True)
         else:
             future.set_result((data, addr))
         return future
 
-    # TODO: provide configurable mechanism of response manipulation
-    def __send_to(self, data, addr, future=None, registered=False):
-        fd = self.socket.fileno()
+    def __sendto(self, data, addr, future=None, registered=False):
+        fd = self.server_sock.fileno()
         if future is None:
             future = self.event_loop.create_future()
         if registered:
@@ -73,10 +82,9 @@ class UdpServer(object):
             return
 
         try:
-            # TODO: add handler for 'data' arg
-            n = self.socket.sendto(data, addr)
+            n = self.server_sock.sendto(data, addr)
         except (BlockingIOError, InterruptedError):
-            self.event_loop.add_writer(fd, self.__send_to, self.event_loop, self.socket, data, addr, future, True)
+            self.event_loop.add_writer(fd, self.__sendto, data, addr, future, True)
         else:
             future.set_result(n)
         return future
