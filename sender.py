491364961a1798924a2b81f5cbcd6472356c5ab0
Alexander Balyshyn <balyszyn@gmail.com>
2019-07-08 21:49:56 +0300

    fix dependencies

--- sender.py
+++ sender.py
@@ -3,12 +3,8 @@ import socket
 UDP_IP = "localhost"
 UDP_PORT = 3553
 MESSAGE = b"Hello, World!"
-
-sock = socket.socket(socket.AF_INET,  # Internet
-
-                     socket.SOCK_DGRAM)  # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-
 print('waiting to receive')
 data, server = sock.recvfrom(4096)
 print('received "%s"' % data)
